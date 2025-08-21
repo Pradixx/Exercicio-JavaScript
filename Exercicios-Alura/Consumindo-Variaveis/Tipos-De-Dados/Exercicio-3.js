417ab1d50c7e8c7754cb0e8d3fbc6020e0ae2db0
Pradixx <dieguittosilva09@gmail.com>
2025-08-20 23:40:12 -0300

    Tipos de dados

---
+++ Exercicios-Alura/Consumindo-Variaveis/Tipos-De-Dados/Exercicio-3.js
@@ -0,0 +1,5 @@
+const banana = "Bananas";
+const quantidade = 10;
+const comendo = false;
+
+console.log("Temos " + quantidade + " " + banana + ", estou comendo: " + comendo);
---
+++ Exercicios-Alura/Consumindo-Variaveis/Tipos-De-Dados/Exercicio-5.js
@@ -0,0 +1,5 @@
+let finado = "Ta morto";
+
+console.log(finado.toUpperCase());
+console.log(finado.toLowerCase());
+console.log(finado.slice());
