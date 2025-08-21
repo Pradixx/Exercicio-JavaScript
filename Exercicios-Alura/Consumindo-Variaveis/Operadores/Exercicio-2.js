6975a464e6ed48715d8b15eda53220ad0545209e
Pradixx <dieguittosilva09@gmail.com>
2025-08-20 23:35:47 -0300

    Operadores

---
+++ Exercicios-Alura/Consumindo-Variaveis/Operadores/Exercicio-2.js
@@ -0,0 +1,15 @@
+let num = 8;
+
+if (num % 2 === 0) {
+    console.log("Esse número é par");
+} else {
+    console.log("Esse número é ímpar");
+}
+
+num = 5;
+
+if (num % 2 === 0) {
+    console.log("Esse número é par");
+} else {
+    console.log("Esse número é ímpar");
+}
---
+++ Exercicios-Alura/Consumindo-Variaveis/Operadores/Exercicio-4.js
@@ -0,0 +1,8 @@
+let morango = true;
+let morando = false;
+
+if (morando === true || morango === true) {
+    console.log("Aprovado");
+}  else {
+    console.log("Reprovado");
+}
