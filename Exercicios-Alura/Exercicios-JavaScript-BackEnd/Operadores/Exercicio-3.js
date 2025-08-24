4bf53232deb56beb14f75a257beb9dcbc930076c
Pradixx <dieguittosilva09@gmail.com>
2025-08-24 01:28:17 -0300

    Organizando

---
+++ Exercicios-Alura/Exercicios-JavaScript-BackEnd/Operadores/Exercicio-3.js
@@ -0,0 +1,11 @@
+let usuario1 = "Diego";
+let usuario1_admin = true;
+
+let usuario2 = "Daniel";
+let usuario2_admin = false;
+
+if (usuario1 === "Diego" && usuario1_admin === true) {
+    console.log("Acesso liberado");
+} else {
+    console.log("Acesso negado");
+}
---
+++ Exercicios-Alura/Exercicios-JavaScript-BackEnd/Operadores/Exercicio-5.js
@@ -0,0 +1,8 @@
+let idade_minima = 14;
+let idade_usuario = 15;
+
+if (idade_usuario > idade_minima) {
+    console.log("Boas compras!")
+} else {
+    console.log("Compra negada");
+}
